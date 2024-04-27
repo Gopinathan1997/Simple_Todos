db65faa7cb02dd015f21cdffbdd05cdb232e2ed3
Gopinathan <144031533+Gopinathan1997@users.noreply.github.com>
2024-04-27 12:27:45 +0530

    Introducing Edit and Save

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,16 +2,34 @@
 import './index.css'
 
 const TodoItem = props => {
-  const {todoItem, onDelete} = props
-  const {id, title} = todoItem
+  const {todoItem, onDelete, onEdit, onSave} = props
+  const {id, title, isEditable} = todoItem
 
   const deleteitem = () => {
     onDelete(id)
   }
+
+  const edititem = () => {
+    onEdit(id)
+  }
+
+  const saveItem = () => {
+    onSave(id)
+  }
+
   return (
     <li className="list">
       <p className="title">{title}</p>
-      <button className="button" onClick={deleteitem} type="button">
+      {isEditable ? (
+        <button className="button save" onClick={saveItem} type="button">
+          Save
+        </button>
+      ) : (
+        <button className="button edit" onClick={edititem} type="button">
+          Edit
+        </button>
+      )}
+      <button className="button delete" onClick={deleteitem} type="button">
         Delete
       </button>
     </li>
